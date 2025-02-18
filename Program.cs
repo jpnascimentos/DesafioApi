5b02c72f192e0f825169c6ca89fa541c26a49467
jpnascimentos <67488329+jpnascimentos@users.noreply.github.com>
2025-02-17 22:29:34 -0300

    Projeto Desafio Novo

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using DesafioApi.Interfaces;
+using DesafioApi.Models;
+using DesafioApi.Repositorios;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DesafioContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoString"));
+});
+
+builder.Services.AddScoped<IClienteRepositorio, ClienteRepositorio>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
